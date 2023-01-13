32124581da203d80c0b6423dddc52c4f114e848a
JYS99 <95613112+JYS-99@users.noreply.github.com>
2023-01-13 18:36:46 -0500

    Add files via upload

---
+++ MMA-RNN/data.py
@@ -0,0 +1,25 @@
+from torch.utils.data import Dataset
+
+class MMECG(Dataset):
+    def __init__(self, dataMat, labelMat, LenMat, SeqLabelMat, OrdMat, numMat, index_list):
+        super().__init__()
+        self.dataMat = dataMat
+        self.labelMat = labelMat
+        self.LenMat = LenMat
+        self.SeqLabelMat = SeqLabelMat
+        self.index_list = index_list
+        self.ordMat = OrdMat
+        self.numMat = numMat
+
+    def __len__(self):
+        return len(self.dataMat)
+
+    def __getitem__(self, idx):
+        id = self.index_list[idx]
+
+        return {'id':self.numMat[id],
+                'ord':self.ordMat[id],
+            'dataMat': self.dataMat[id],
+                'labelMat': self.labelMat[id],
+                'lenMat': self.LenMat[id],
+                'seqLabelMat': self.SeqLabelMat[id]}
